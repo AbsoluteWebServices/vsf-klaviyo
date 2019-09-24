c817a104dcfc187a212d7c1e4cd5916872ba08ef
Dmitry Kozachek <dmitry.kozachek@gmail.com>
2019-09-24 19:29:45 +0300

    fix(helpers): Fix base64 helper es-lint errors

--- helpers/webtoolkit.base64.js
+++ helpers/webtoolkit.base64.js
@@ -64,7 +64,7 @@ export const Base64 = {
     let enc1, enc2, enc3, enc4
     let i = 0
 
-    input = input.replace(/[^A-Za-z0-9\+\/\=]/g, '')
+    input = input.replace(/[^A-Za-z0-9+/=]/g, '')
 
     while (i < input.length) {
       enc1 = this._keyStr.indexOf(input.charAt(i++))
